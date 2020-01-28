a3cd76c312778819526af73b2ff2174ab381f37c
Jeremy FROMINVILLE <j.e.fromin@gmail.com>
2020-01-28 17:14:26 +0100

    Ajout des fonctions supplementaires (manque la fonction de vérification de la disponibilité des chambres

--- presentation.js
+++ presentation.js
@@ -16,10 +16,14 @@ function startMenu(){
         Menu
 ~.~.~.~.~.~.~.~.~.~.~
 1. Lister les clients
+2. Ajouter un client
+3. Rechercher un client par nom
+4. Vérifier la disponibilité d'une chambre
 99. Sortir
 
 Veuillez saisir votre choix:
         `;
+    var moduleService= require("./service.js");
 
     // récupération de la saisie utilisateur
     rl.question(chaineMenu, function(saisie) {
@@ -27,13 +31,51 @@ Veuillez saisir votre choix:
         console.log(`Votre choix : ${saisie}`);
         switch(saisie){
             case "1":
-                var moduleService= require("./service.js");
                 moduleService.listerClients(function(data){
-                    data.forEach(client => {
-                        console.log(` #${client.uuid} ${client.prenoms} ${client.nom}`);
-                    });
+                    console.log("Clients trouvés: ");
+                    afficherListeClients(data);
                     startMenu();
                 });
+                break;
+
+            case "2":
+                rl.question("Veuillez saisir le nom du nouveau client: ", function(nom) {
+                    console.log(nom); 
+                    rl.question("Veuillez saisir le (les) prénom(s) du nouveau client: ", function(prenoms) {
+                        console.log(prenoms);
+
+                        moduleService.creerClient(nom, prenoms, function(data){
+                            console.log("Enregistrement du client...");
+                            console.log(data);
+                            startMenu();
+                        });
+
+                    });
+                });                
+                break;
+
+            case "3":
+                rl.question("Veuillez saisir le nom du client: ", function(nom) {
+                    moduleService.rechercherClientParNom(nom, function(data){
+                        console.log("Clients trouvés: ");
+                        afficherListeClients(data);
+                        startMenu();
+                    });
+                });
+                break;
+
+            case "4": /* A terminer */
+                rl.question("Veuillez saisir la période pour laquelle vous voulez vérifier la disponibilité des chambres.\nDate de début: ", function(dateDebut) {
+                    rl.question("Date de fin: ", function(dateFin) {
+                        console.log(` Du ${dateDebut} au ${dateFin} :`);
+
+                        moduleService.verifierDisponibilite(dateDebut, dateFin, function(data){
+                            console.log(data);
+                            startMenu();
+                        });
+
+                    });
+                });                
                 break;
 
             case "99":
@@ -43,6 +85,12 @@ Veuillez saisir votre choix:
         }
     });
 
+}
+
+function afficherListeClients(listeClients){
+    listeClients.forEach(client => {
+        console.log(`=> #${client.uuid} ${client.prenoms} ${client.nom}`);
+    });
 }
 
 exports.start= start;
--- service.js
+++ service.js
@@ -1,7 +1,8 @@
+// importation de la librairie request
+// recherche par défaut dans le répertoire node_modules
+var request = require('request');
+
 function listerClients(callbackFn){
-    // importation de la librairie request
-    // recherche par défaut dans le répertoire node_modules
-    var request = require('request');
     // TODO visualiser l'adresse https://jsonplaceholder.typicode.com/posts avec votre navigateur.
     // les données sont exposées au format JSON.
     // Envoie de la requête http
@@ -16,4 +17,42 @@ function listerClients(callbackFn){
     });
 }
 
+function rechercherClientParNom(nom, callbackFn){
+    request('http://localhost:8080/clients?nom='+nom, { json: true }, function(err, res, data) {
+        if (err) { return console.log('Erreur', err); }
+        callbackFn(data);
+    });
+}
+
+function creerClient(nom, prenoms, callbackFn){
+    request.post({
+        url: 'http://localhost:8080/clients',
+        body: {
+            "nom":nom,
+            "prenoms":prenoms
+        },
+        json: true
+    }, function(err, response, status){
+        if (err) { return console.log('Erreur', err); }
+        callbackFn(status);
+    });
+}
+
+function verifierDisponibilite(dateDebut, dateFin, callbackFn){
+    request.post({
+        url: 'http://localhost:8080/disponibilite',
+        body: {
+            "dateDebut":dateDebut,
+            "dateFin":dateFin
+        },
+        json: true
+    }, function(err, response, data){
+        if (err) { return console.log('Erreur', err); }
+        callbackFn(data);
+    });
+}
+
 exports.listerClients= listerClients;
+exports.rechercherClientParNom= rechercherClientParNom;
+exports.creerClient= creerClient;
+exports.verifierDisponibilite= verifierDisponibilite;
